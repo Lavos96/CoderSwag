f6540322c21f94cbbf3f5ad5455fa801455dae93
Michał Kowalczyk <lavos1996@o2.pl>
2018-12-10 17:43:34 +0100

    adapter with viewHolder

--- app/src/main/java/com/kowalczyk/michal/coderswag/Adapters/CategoryAdapter.kt
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Adapters/CategoryAdapter.kt
@@ -16,20 +16,33 @@ class CategoryAdapter(context:Context,categories:List<Category>): BaseAdapter() 
     val categories=categories
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val categoryView:View
+        val holder:ViewHolder
 
-        categoryView=LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
-        val categoryImage:ImageView=categoryView.findViewById(R.id.categoryImage)
-        val categoryName:TextView=categoryView.findViewById(R.id.categoryName)
+        //jesli convertview jest pusty to go zainicjuj
+        if(convertView==null) {
+            //LayoutInflater przerabia ten pliczek xml zeby mogl z niego korzystac prz tworzeniu listview
+            categoryView=LayoutInflater.from(context).inflate(R.layout.category_list_item,null)
+            //stworzenie ViewHoldera po to zeby obliczenia nie robily sie po kazdym przewiniecu listy
+            //
+            holder=ViewHolder()
+            //przypisanie do categoryView holdera categoryView z orginalnego widoku(categoryView ktory przy pomocy LayoutInflatera
+            // przerobil plik xml category_list_item na swoj uzytek)
+            holder.categoryImage=categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName=categoryView.findViewById(R.id.categoryName)
+
+            //nadanie unikatowego id (taga) dla categoryView ktory bedzie rowny holderowi
+            categoryView.tag=holder
+        }else{
+            holder=convertView.tag as ViewHolder
+            categoryView=convertView
+        }
 
         val category=categories[position]
-
+        //pobranie id konkretnego jednego elementy ListView
         val resourceId=context.resources.getIdentifier(category.image,"drawable",context.packageName)
-        categoryImage.setImageResource(resourceId)
-        println(resourceId)
+        holder.categoryImage?.setImageResource(resourceId)
 
-
-        categoryName.text=category.title
-
+        holder.categoryName?.text=category.title
 
         return categoryView
     }
@@ -44,5 +57,12 @@ class CategoryAdapter(context:Context,categories:List<Category>): BaseAdapter() 
 
     override fun getCount(): Int {
        return categories.count()
+    }
+//stworzenie klasy ViewHoldera pozwala ona ponownie uzywac te elementy ListView ktore juz byly wczesniej wczytane
+    //jest on robiony zeby za kazdym razem sie nie musialo od nowa generowac
+    private class ViewHolder{
+    //w tym pliku xml (widoku) jest obrazek categoryImage oraz text categoryName
+        var categoryImage:ImageView?=null
+        var categoryName:TextView?=null
     }
 }
--- app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kowalczyk.michal.coderswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.kowalczyk.michal.coderswag.Adapters.CategoryAdapter
 import com.kowalczyk.michal.coderswag.Model.Category
 import com.kowalczyk.michal.coderswag.R
 import com.kowalczyk.michal.coderswag.Services.DataService
@@ -10,14 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter:ArrayAdapter<Category>
+    lateinit var adapter:CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter= CategoryAdapter(this,DataService.categories)
         categoryListView.adapter=adapter
     }
 }
