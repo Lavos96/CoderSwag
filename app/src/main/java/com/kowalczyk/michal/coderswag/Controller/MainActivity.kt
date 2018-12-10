cdb16163d8147f34dcda9359244ede21c26cbca6
Michał Kowalczyk <lavos1996@o2.pl>
2018-12-10 16:56:52 +0100

    adapter 1 lesson

--- app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.kowalczyk.michal.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.kowalczyk.michal.coderswag.Model.Category
 import com.kowalczyk.michal.coderswag.R
+import com.kowalczyk.michal.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter:ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter= ArrayAdapter(this,android.R.layout.simple_list_item_1,
+            DataService.categories)
+        categoryListView.adapter=adapter
     }
 }
