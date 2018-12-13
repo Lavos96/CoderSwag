0235910bf7c68c0bd44668d297db094b58f23c62
Michał Kowalczyk <lavos1996@o2.pl>
2018-12-13 13:50:08 +0100

    Recycler View and ListView with ViewHolder

--- app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
@@ -2,8 +2,12 @@ package com.kowalczyk.michal.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.kowalczyk.michal.coderswag.Adapters.CategoryAdapter
+import com.kowalczyk.michal.coderswag.Adapters.CategoryRecycleAdapter
 import com.kowalczyk.michal.coderswag.Model.Category
 import com.kowalczyk.michal.coderswag.R
 import com.kowalczyk.michal.coderswag.Services.DataService
@@ -11,13 +15,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter:CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter= CategoryAdapter(this,DataService.categories)
-        categoryListView.adapter=adapter
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
+        categoryListView.adapter = adapter
+
+        val layoutManager=LinearLayoutManager(this)
+        categoryListView.layoutManager=layoutManager
+
+        //jesli nasze komorki w recycleView nie zmieniaja rozmiarow to dla optymalizacji linia ponizej
+        categoryListView.setHasFixedSize(true)
+
+
+        //Do ListView Adapter z viewHolderem
+        /*categoryListView.setOnItemClickListener { parent, view, position, id ->
+            val category = DataService.categories[position]
+            Toast.makeText(this, "you clicked on ${category.title} cell", Toast.LENGTH_SHORT).show()
+        }*/
     }
 }
