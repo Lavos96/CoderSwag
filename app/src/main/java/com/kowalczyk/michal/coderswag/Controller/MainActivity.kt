847927211280a2d3de5f2bd2dcdf76610c7d83d6
Michał Kowalczyk <lavos1996@o2.pl>
2018-12-16 15:03:20 +0100

    Lamda przy przechodzeniu do nowego view z pozizomu recyclerView

--- app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Controller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kowalczyk.michal.coderswag.Controller
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -11,6 +12,7 @@ import com.kowalczyk.michal.coderswag.Adapters.CategoryRecycleAdapter
 import com.kowalczyk.michal.coderswag.Model.Category
 import com.kowalczyk.michal.coderswag.R
 import com.kowalczyk.michal.coderswag.Services.DataService
+import com.kowalczyk.michal.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +23,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this,DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories){ category ->
+            val productIntent=Intent(this,ProductsActivity::class.java)
+            productIntent.putExtra(EXTRA_CATEGORY,category.title)
+            startActivity(productIntent)
+        }
         categoryListView.adapter = adapter
 
         val layoutManager=LinearLayoutManager(this)
---
+++ app/src/main/java/com/kowalczyk/michal/coderswag/Utilities/Constants.kt
@@ -0,0 +1,3 @@
+package com.kowalczyk.michal.coderswag.Utilities
+
+const val EXTRA_CATEGORY="category"
